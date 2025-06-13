6e0c83d664d7c9c2c46c0a12f4f67cc80d50a777
Robert Bhanu Prasad <bhanu.robert@artofliving.org>
2025-06-13 12:44:14 +0530

    feature:ccn import is updated

--- src/ui/input.tsx
+++ src/ui/input.tsx
@@ -1,6 +1,6 @@
 import * as React from "react"
 import { cn } from "../lib/utils"
-import { ccn } from "src/lib/CommonComponentsUtil";
+import { ccn } from "../lib/CommonComponentsUtil";
 
 export interface InputProps
   extends React.InputHTMLAttributes<HTMLInputElement> {
